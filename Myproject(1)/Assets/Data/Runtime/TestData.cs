2d9985cc1b26015384414de217df2b589c215f9c
papermeat <junemo1004@gmail.com>
2025-06-11 17:45:35 +0900

    [Add]250611_Package
    저의 파일이 날아가 버려서 강사님 패키지파일을 추가하였습니다...

---
+++ Myproject(1)/Assets/Data/Runtime/TestData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+///
+/// !!! Machine generated code !!!
+/// !!! DO NOT CHANGE Tabs to Spaces !!!
+/// 
+[System.Serializable]
+public class TestData
+{
+  [SerializeField]
+  int id_i;
+  public int ID_I { get {return id_i; } set { this.id_i = value;} }
+  
+  [SerializeField]
+  string name;
+  public string Name { get {return name; } set { this.name = value;} }
+  
+}
